add888c88c5e7f4e4bf1a838496b2dd7267c6899
Tom Hutchinson <git@tomisme.com>
2016-01-25 11:52:13 +0800

    separate buttons from gear element in spin test

--- src/tzolkin/devcards/art.cljs
+++ src/tzolkin/devcards/art.cljs
@@ -92,27 +92,30 @@
             :tooth-height-factor 1.1})
   {:inspect-data true})
 
-(def gear-rotation-atom (rg/atom 0))
+(def spin-test-atom (rg/atom 0))
 
-(defn worker-gear-spin-test
+(defn spinning-worker-gear
   [{:keys [workers actions on-worker-click]}]
-  (let [rotation-spring (anim/spring gear-rotation-atom)]
+  (let [rotation-spring (anim/spring spin-test-atom)
+        workers [:blue nil :blue :red nil nil :red nil nil nil]]
     (fn []
-      [:center
-        [:button {:on-click #(swap! gear-rotation-atom + (/ 360 10))}
-          "Spin the gear! (turn: " (str (/ @gear-rotation-atom 36)) ")"]
-        [:svg {:width 300 :height 300}
-          [art/gear-el {:cx 150
-                        :cy 150
-                        :r 75
-                        :teeth 10
-                        :tooth-height-factor 1.15
-                        :tooth-width-factor 0.75
-                        :workers workers
-                        :gear :tik
-                        :rotation @rotation-spring
-                        :on-worker-click on-worker-click}]]])))
+      [:svg {:width 300 :height 300}
+        [art/gear-el {:cx 150
+                      :cy 150
+                      :r 75
+                      :teeth 10
+                      :tooth-height-factor 1.15
+                      :tooth-width-factor 0.75
+                      :workers workers
+                      :gear :tik
+                      :rotation @rotation-spring}]])))
 
-(defcard-rg worker-gear-spin-test
-  [worker-gear-spin-test
-    {:workers [:blue nil :blue :red nil nil :red nil nil nil]}])
+(defcard-rg spinning-worker-gear-test
+  [:div
+    [:button {:on-click #(swap! spin-test-atom + (/ 360 10))}
+      "Spin the gear!"]
+    (for [[k v] (get logic/game-spec :gears)]
+      [:button (:name v)])
+    [spinning-worker-gear]]
+  spin-test-atom
+  {:inspect-data true})
