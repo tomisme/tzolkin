2affc2cfcb2efab6f88a23faac14882df48e1256
Tom Hutchinson <git@tomisme.com>
2016-03-12 12:44:24 +0800

    devtools don't need to be installed?

--- src/tzolkin/dev_core.cljs
+++ src/tzolkin/dev_core.cljs
@@ -1,8 +1,11 @@
 (ns tzolkin.dev-core
   (:require
    [tzolkin.devcards.main]
-   [devtools.core :as devtools]))
+   [devtools.core :as devtools]
+   [tzolkin.utils :refer [log]]))
 
-(devtools/install!)
+#_(devtools/install!)
+
+#_(log js/window)
 
 (enable-console-print!)
